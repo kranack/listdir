4d155e172200711455281e8c3672270b262b0ecc
Damien Calesse <damien.calesse@gmail.com>
2023-12-13 17:11:54 -0500

    Remove background color on body

--- resources/views/list/index.blade.php
+++ resources/views/list/index.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
   </head>
-  <body class="has-background-white-bis" style="height: 100vh">
+  <body style="height: 100vh">
     <section class="section">
       <div class="columns is-centered">
         <div class="column is-half" style="display: flex; justify-content: center">
