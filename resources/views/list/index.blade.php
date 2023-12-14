50e1cd075497e23ee66e477af5394e9d6e500f8a
Damien Calesse <damien.calesse@gmail.com>
2023-12-13 22:08:20 -0500

    Re-organize CSS

--- resources/views/list/index.blade.php
+++ resources/views/list/index.blade.php
@@ -6,21 +6,50 @@
 
     <title>VOD Calesse</title>
 
-    <link rel="icon" href="/favicon.svg">
+    <link rel="icon" href="/favicon.svg" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" />
+
+    <style type="text/css">
+      body {
+        height: 100vh;
+      }
+
+      .logo {
+        display: flex;
+        justify-content: center;
+        content-visibility: auto;
+      }
+
+      .logo > figure {
+        height: 250px;
+        width: 250px;
+      }
+
+      .viewer {
+        border-radius: 5px;
+        padding: 10px;
+      }
+
+      .viewer-controls {
+        border-bottom: 1px dashed;
+        margin-bottom: 5px;
+        padding-bottom: 1px;
+      }
+
+    </style>
   </head>
-  <body style="height: 100vh">
+  <body>
     <section class="section">
       <div class="columns is-centered">
-        <div class="column is-narrrow" style="display: flex; justify-content: center; content-visibility: auto">
-          <figure class="image" style="height: 250px; width: 250px">
+        <div class="column is-narrrow logo">
+          <figure class="image">
             <img src="{{ asset('/logos/damien_vod.png') }}" loading="async">
           </figure>
         </div>
       </div>
-      <div class="container" style="border-radius: 5px; padding: 10px">
-        <div style="border-bottom: 1px dashed; margin-bottom: 5px; padding-bottom: 1px">
+      <div class="container viewer">
+        <div class="viewer-controls">
           @if ($has_previous)
             <a href="{{ dirname($root) }}">
               <span class="icon">
